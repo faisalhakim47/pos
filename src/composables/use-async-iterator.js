7c0766e2db8966cdd1296ef31b1e9ccdf1e02982
Faisal Hakim <faisalhakim47@gmail.com>
2025-06-10 07:28:55 +0700

    checkpoint

--- src/composables/use-async-iterator.js
+++ src/composables/use-async-iterator.js
@@ -1,11 +1,12 @@
 // @ts-check
 
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 /**
  * @template T
  * @template P
  * @typedef {object} UseAsyncIteratorState
+ * @property {boolean} ready
  * @property {T | undefined} state
  * @property {unknown} error
  * @property {(...args: Array<P>) => Promise<void>} run
@@ -19,12 +20,20 @@ import { reactive } from 'vue';
  * @return {UseAsyncIteratorState<T | R, P>}
  */
 export function useAsyncIterator(fn) {
+  const ready = ref(true);
   const data = reactive({
+    get ready() {
+      return ready.value;
+    },
     state: undefined,
     error: undefined,
     /**  @param {Array<P>} args */
     async run(...args) {
+      if (!ready.value) {
+        throw new Error('useAsyncIterator only supports one active iterator at a time');
+      }
       try {
+        ready.value = false;
         data.state = undefined;
         data.error = undefined;
         for await (const value of fn(...args)) {
@@ -34,6 +43,9 @@ export function useAsyncIterator(fn) {
       catch (error) {
         data.state = undefined;
         data.error = error;
+      }
+      finally {
+        ready.value = true;
       }
     },
   });
